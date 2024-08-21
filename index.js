5881858ff9a99c8c96885dff085465a57c8b5c6f
Joseph Crump <josephcrump79@yahoo.com>
2024-08-21 18:17:52 -0500

    JS functions continued

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+function saturdayFun(parameter = "roller-skate")
+{
+//    console.log (parameter);
+//return "This Saturday, I want to "+parameter+"!";
+return `This Saturday, I want to ${parameter}!`; 
+}
+//saturdayFun ();
+//saturdayFun ("music")
+
+function mondayWork(parameter = "go to the office")
+{
+
+return `This Monday, I will ${parameter}.`;  
+     
+}
+
+function wrapAdjective(flair = "*"){
+    return function (adjective = "special") {
+            return `You are ${flair}${adjective}${flair}!`
+    }
+}
+/*
+const inner = wrapAdjective("~");
+console.log(inner)
+const res = inner("Amazing") 
+console.log(res)*/
